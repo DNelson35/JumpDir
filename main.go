801ad38ad9315c16283dc94a45440ae74fb8c5fa
Damien Nelson <dhnisaman@gmail.com>
2024-08-12 23:42:42 -0400

    add help flag update check for args, update function notes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -9,7 +10,18 @@ import (
 	"slices"
 )
 
-// jumpDirectory takes in the name (case insensitive) of the directory you want to find and a starting directory to begin the search from. This will start a depth-first search from the starting directory to locate the directory passed in in the first argument. the return will be the full path to the directory.
+func printUsage() {
+	    fmt.Println("Usage: go run main.go <target_directory> [<starting_point>]")
+    fmt.Println()
+    fmt.Println("Arguments:")
+    fmt.Println("  <target_directory>    Target directory (required)")
+    fmt.Println("  [<starting_point>]    Starting point (optional)")
+    fmt.Println()
+    fmt.Println("Flags:")
+    flag.PrintDefaults()
+}
+
+// jumpDirectory takes in the name (case insensitive) of the directory you want to find and a starting directory to begin the search from. This will start a depth-first search from the starting directory to locate the directory passed in in the first argument. the return will be the full path to the directory. Pass . as the second argument if you want to start the search form your current directory.
 func jumpDirectory(name string, currDir string) string {
 	os.Chdir(currDir)
 	cleanPaths, path := getDirs(currDir, name)
@@ -67,14 +79,25 @@ func getDirs(dir string, name string)([]string, string){
 }
 
 func main(){
-
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go <name> <current_directory>")
+	var help bool
+	flag.BoolVar(&help, "help", false, "Show help information")
+	flag.BoolVar(&help, "h", false, "Show help information") 
+	flag.Parse()
+	
+	if help {
+		printUsage()
+		os.Exit(0)
+	}
+	
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Println("Error: <target_directory> and <starting_point> are required.")
+		printUsage()
 		os.Exit(1)
 	}
 
-	name := os.Args[1]
-	currDir := os.Args[2]
+	name := args[0]
+	currDir := args[1]
 
 	result := jumpDirectory(name, currDir)
 	fmt.Println(result)
